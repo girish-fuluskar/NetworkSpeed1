3bb3cb7ee7f8900f9f9f8441a7e04f994ab220a3
Girish <girish.fuluskar@gmail.com>
2018-09-17 19:46:09 +0530

    changes 13

--- src/android/NetworkSpeed1.java
+++ src/android/NetworkSpeed1.java
@@ -56,7 +56,7 @@ public class NetworkSpeed1 extends CordovaPlugin {
     private Context mContext;
 
 
-    @Override
+    //@Override
     public boolean execute(String action, Context context, CallbackContext callbackContext) throws JSONException {
         if(action.equals("startServiceNow")) {
             this.startServiceNow(context);
